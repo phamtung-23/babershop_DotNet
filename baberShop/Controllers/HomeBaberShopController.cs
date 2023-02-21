1cf4a427bea7a36978e5137e6b6cc254b0665ac1
phamtung-23 <=>
2023-02-21 11:16:58 +0700

    upload project

---
+++ baberShop/Controllers/HomeBaberShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baberShop.Controllers
+{
+    public class HomeBaberShopController : Controller
+    {
+        // GET: HomeBaberShop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
